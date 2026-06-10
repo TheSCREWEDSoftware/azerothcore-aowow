24e0350042edc2f33d8b679355931ad152bb6490
FlyingArowana <TheSCREWEDSoftware@users.noreply.github.com>
2026-06-10 19:19:32 +0100

    guard against invalid requiredHonorRank lookup
    Closes: https://github.com/azerothcore/aowow/issues/78

--- includes/types/item.class.php
+++ includes/types/item.class.php
@@ -907,8 +907,8 @@ class ItemList extends BaseType
         }
 
         // required honorRank (not used anymore)
-        if ($rhr = $this->curTpl['requiredHonorRank'])
-            $x .= Lang::game('requires', [implode(' / ', Lang::game('pvpRank', $rhr))]).'<br />';
+        if (($rhr = $this->curTpl['requiredHonorRank']) && is_array($pvpRank = Lang::game('pvpRank', $rhr)))
+            $x .= Lang::game('requires', [implode(' / ', $pvpRank)]).'<br />';
 
         // required CityRank..?
         // what the f..
