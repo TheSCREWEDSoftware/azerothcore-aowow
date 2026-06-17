cd753e6fc8d0ae65eba22001bbd403dbaffa28bc
FlyingArowana <16946913+TheSCREWEDSoftware@users.noreply.github.com>
2026-06-17 12:42:21 +0100

    Cleanup - Fix nicePath() mangling absolute *nix paths
    Paths not under /home/ had their leading slash stripped, making them
    relative. Only ~/ and ~user/ need expansion; other absolute paths pass
    through unchanged.
    
    Thanks @Sarjuuk for pointing it out

--- includes/utilities.php
+++ includes/utilities.php
@@ -393,8 +393,7 @@ abstract class CLI
                 $path = '/home/'.substr($path, 1);
             else if (substr($path, 0, 2) == '~/')
                 $path = getenv('HOME').substr($path, 1);
-            else if ($path[0] == DIRECTORY_SEPARATOR && substr($path, 0, 6) != '/home/')
-                $path = substr($path, 1);
+            // absolute paths (starting with /) are left as-is
         }
 
         return $path;
