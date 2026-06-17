6ac9508e2641facb00d1ea95db0c7f617524e7f4
FlyingArowana <16946913+TheSCREWEDSoftware@users.noreply.github.com>
2026-06-17 12:35:05 +0100

    Cleanup - Declare $poolLegend as class property in ObjectPage
    $this->poolLegend was assigned dynamically without a class-level
    declaration, causing the same PHP 8.2/9 deprecation/fatal as gossipCndResult.
    Added protected $poolLegend = null; to ObjectPage.

--- pages/object.php
+++ pages/object.php
@@ -12,6 +12,7 @@ class ObjectPage extends GenericPage
 
     protected $pageText      = [];
     protected $relBoss       = null;
+    protected $poolLegend    = null;
 
     protected $type          = Type::OBJECT;
     protected $typeId        = 0;
